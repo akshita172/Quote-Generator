f1b195e44f052380502b83ca506b6a61d272e6af
Akshita Shukla <akshita002mca22@igdtuw.ac.in>
2023-08-17 22:23:08 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDom from 'react-dom';
+// import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const index = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+ReactDom.render(<App />, document.getElementById('root'));
+export default index
